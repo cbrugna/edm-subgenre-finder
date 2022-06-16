8b70eb3b4f571aa50146040888276acf8adef30e
Casey Brugna <cbrugna1@binghamton.edu>
2022-06-16 00:44:54 -0400

    started

---
+++ cred.py
@@ -0,0 +1,4 @@
+client_id= '05ce8da1369f40afa35d5d8a0c88aec7'
+client_secret = '07c34a7c3bf348fbad425f2c9a57b81b'
+redirect_url = 'http::/www.google.com'
+user_url = 'https://open.spotify.com/user/cbrugna?si=a08e499a0b5145f1'
